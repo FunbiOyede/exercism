3a69f48b43ce3f149ebaed5f928822fedd20d31e
Oyede Oluwafunbi <oyedetobi49@gmail.com>
2020-04-12 23:22:18 +0100

    resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+  export const colorCode = (param) => {
+
+ const color = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+  const number = color.indexOf(param.toLowerCase());
+    return number
+
+
+};
+
+  export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
