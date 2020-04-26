d8cd13a3f542de603a7c9b375c1d89a4f77a4e59
Oyede Oluwafunbi <oyedetobi49@gmail.com>
2020-04-26 01:09:28 +0100

    space age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,18 @@
+const earthPeriodInSeconds = 31557600;
+
+const orbitalRatio = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+export const age = (planet, seconds) => {
+  const period = earthPeriodInSeconds * orbitalRatio[planet];
+
+  return parseFloat((seconds / period).toFixed(2));
+};
