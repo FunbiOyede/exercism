f60f42811a603f85d11fc9741df8c1acbd01bd61
Oyede Oluwafunbi <oyedetobi49@gmail.com>
2020-05-11 00:58:47 +0100

    word count completion

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,16 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
- const countWords = (phrase) => {
-   let counter = 0;
-   const word = phrase.split(" ");
-   for(let i = 0; i < word.length; i++){
-     if(word[i] === phrase){
-       console.log("balls")
+/**
+ * 
+ * @param {string} phrase 
+ * @description checks the number of duplicate word in a sentence
+ * @example "Monkey do, Monkey go" => {monkey:2,go:1,do:1}
+ */
+
+ export const countWords = (phrase) => {
+  let counter = {};
+  let  word = phrase.toLocaleLowerCase().match(/\b\w+('\w)?\b/g)
+  word.forEach(element => {
+     if(!counter.hasOwnProperty(element)){
+       counter[element] = 1
+     }else{
+      ++counter[element]
      }
-   }
-  
+  });
+  return counter
 };
-
-
-countWords("funbi come, funbi go?!")
